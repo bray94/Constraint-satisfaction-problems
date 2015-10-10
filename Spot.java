5cdbf9fc3379b2522f70215b96eb1ab9487d77fd
Braydenturner <mailto:Braydenturner94@gmail.com>
2015-10-10 22:43:36 +0000

    File to contain all the letters possible for each spot in the puzzle. Similar to Categories but instead of holding all the words for each it will  hold all the available letters.

---
+++ Spot.java
@@ -0,0 +1,40 @@
+package src.mp2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Spots
+{
+	// _________________________ Spot Number __________________________________
+
+	/**
+	 * The number of the spot in puzzle between 1 and puzzle size
+	 */
+	private int spotNumber;
+
+	/**
+	 * Get the spot number
+	 */
+	public int getSpotNumber()
+	{
+		return spotNumber;
+	}
+
+	/**
+	 * Set the spot number
+	 */
+	public void setSpotNumber(int num)
+	{
+		spotNumber = num;
+	}
+
+	// ___________________________________ Letter LIST __________________________________
+
+
+
+
+
+
+}
